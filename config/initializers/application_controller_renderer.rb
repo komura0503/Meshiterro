1828164b63dc4764216f2f4a2ebafed588752a44
supernemutai <tokikake66@gmail.com>
2021-09-16 16:49:16 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'db885c2bbec14cdef0c79015037e79dc2c3f6dfa0c6630469370e33189e97bfea65a3a2521bbe1a4292e4bf1918fca9d849c78ad0e30c8f2d224b3d6f086d225'
